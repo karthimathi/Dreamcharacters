a35462d415a2f59f19a5a400016059d3a33d3fb5
Karthick Kumar Mathivanan <76002821+karthimathi@users.noreply.github.com>
2026-05-13 23:45:42 +0530

    Update ScrollAnimations.jsx

--- Public/src/components/effects/ScrollAnimations.jsx
+++ Public/src/components/effects/ScrollAnimations.jsx
@@ -1,1 +1,31 @@
+import { useEffect } from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
+gsap.registerPlugin(ScrollTrigger)
+
+const ScrollAnimations = () => {
+  useEffect(() => {
+    // Fade in sections on scroll
+    gsap.utils.toArray('section, .glass-card, .grid > div').forEach((element) => {
+      gsap.fromTo(element,
+        { opacity: 0, y: 50 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 1,
+          scrollTrigger: {
+            trigger: element,
+            start: 'top 85%',
+            end: 'bottom 60%',
+            toggleActions: 'play none none reverse',
+          },
+        }
+      )
+    })
+  }, [])
+
+  return null
+}
+
+export default ScrollAnimations
